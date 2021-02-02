6faa2c57a7b05460caba53ec2cfb7882ab958032
Juan C <juanceballos@pursuit.org>
2021-02-01 19:06:18 -0500

    ring buffer queue

--- DSA-3rd-Ed.playground/Pages/Ch8-Queues.xcplaygroundpage/Contents.swift
+++ DSA-3rd-Ed.playground/Pages/Ch8-Queues.xcplaygroundpage/Contents.swift
@@ -11,9 +11,9 @@ public protocol Queue {
 
 // Nodes, and DoublyLinkedList
 public class Node<T> {
-    private var val: T
-    private var next: Node?
-    private var prev: Node?
+    var val: T
+    var next: Node?
+    weak var prev: Node?
     
     public init(val: T) {
         self.val = val
@@ -31,26 +31,58 @@ extension Node: CustomStringConvertible {
 }
 
 public struct DoublyLinkedList<T> {
-    private var head: Node<T>?
-    private var tail: Node<T>?
+    private (set) var head: Node<T>?
+    private (set) var tail: Node<T>?
     
-    public init() {
-        
+    public var isEmpty: Bool {
+        return head == nil
     }
     
-    private var isEmpty: Bool {
-        return head == nil
+    public var first: Node<T>? {
+        return head
+    }
+    
+    public var last: Node<T>? {
+        return tail
     }
     
     public mutating func enqueue(val: T) {
         let newNode = Node(val: val)
         
-        if isEmpty {
+        guard let lastNode = tail else {
             head = newNode
-            
-        } else {
-            
+            tail = newNode
+            return
         }
+        
+        lastNode.next = newNode
+        newNode.prev = lastNode
+        tail = newNode
+    }
+    
+    public mutating func remove(node: Node<T>?) -> T? {
+        let prev = node?.prev
+        let next = node?.next
+        
+        if let prev = prev {
+            prev.next = next
+        } else {
+            head = next
+        }
+        next?.prev = prev
+        if next == nil {
+            tail = prev
+        }
+        node?.prev = nil
+        node?.next = nil
+        
+        return node?.val
+    }
+}
+
+extension QueueLinkedList: CustomStringConvertible {
+    public var description: String {
+        String(describing: list)
     }
 }
 
@@ -78,17 +110,47 @@ public struct QueueArray<T>: Queue {
 }
 
 extension QueueArray: CustomStringConvertible {
-  public var description: String {
-    String(describing: array)
-  }
+    public var description: String {
+        String(describing: array)
+    }
 }
 
 // Doubly Linke List Implementation
 public class QueueLinkedList<T>: Queue {
     private var list = DoublyLinkedList<T>()
     public init() {}
+    
+    public var peek: T? {
+        list.first?.val
+    }
+    
+    public var isEmpty: Bool {
+        list.isEmpty
+    }
+    
+    public func enqueue(_ element: T) -> Bool {
+        list.enqueue(val: element)
+        return true
+    }
+    
+    public func dequeue() -> T? {
+        guard !list.isEmpty, let element = list.first else {
+            return nil
+        }
+        
+        return list.remove(node: element)
+    }
 }
 
+var listQueue = QueueLinkedList<String>()
+listQueue.enqueue("Jordan")
+listQueue.enqueue("Jeff")
+listQueue.enqueue("Joseph")
+listQueue
+print(listQueue)
+listQueue.dequeue()
+print(listQueue)
+print(listQueue.peek)
 
 //--------------------------------TEST---------------------------------------------
 var queue = QueueArray<String>()
@@ -100,3 +162,93 @@ queue.dequeue()
 queue
 queue.peek
 
+public class RingBuffer<T> {
+    public var array: [T?]
+    public var readIndex = 0
+    public var writeIndex = 0
+    
+    init(count: Int) {
+        array = [T?](repeating: nil, count: count)
+    }
+    
+    public var availableSpaceForReading: Int {
+        return writeIndex - readIndex
+    }
+    
+    public var availableSpaceForWriting: Int {
+        return array.count - availableSpaceForReading
+    }
+    
+    public var isFull: Bool {
+        return availableSpaceForWriting == 0
+    }
+    
+    public var isEmpty: Bool {
+        return availableSpaceForReading == 0
+    }
+    
+    public var first: T? {
+        return array[readIndex]
+    }
+    
+    public func read() -> T? {
+        if !isEmpty {
+            let element = array[readIndex % array.count]
+            readIndex += 1
+            return element
+        } else {
+            return nil
+        }
+    }
+    
+    public func write(_ element: T) -> Bool {
+        if !isFull {
+            array[writeIndex % array.count] = element
+            writeIndex += 1
+            return true
+        } else {
+            return false
+        }
+    }
+    
+}
+
+public struct QueueRingBuffer<T>: Queue {
+    private var ringBuffer: RingBuffer<T>
+    
+    public init(count: Int) {
+        ringBuffer = RingBuffer<T>(count: count)
+    }
+    
+    public var isEmpty: Bool {
+        ringBuffer.isEmpty
+    }
+    
+    public var peek: T? {
+        ringBuffer.first
+    }
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        ringBuffer.write(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        ringBuffer.read()
+    }
+    
+}
+
+extension QueueRingBuffer: CustomStringConvertible {
+    public var description: String {
+        String(describing: ringBuffer)
+    }
+}
+
+var queueRB = QueueRingBuffer<String>(count: 10)
+queueRB.enqueue("Ray")
+queueRB.enqueue("Brian")
+queueRB.enqueue("Eric")
+queueRB
+queueRB.dequeue()
+queueRB
+queueRB.peek
