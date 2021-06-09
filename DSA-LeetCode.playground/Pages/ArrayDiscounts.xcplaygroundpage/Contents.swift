af3a8922fbe3640130444494468981d770f5dab9
Juan C <juanceballos@pursuit.org>
2021-06-09 16:03:48 -0400

    stack solution prices discount final prices

--- DSA-LeetCode.playground/Pages/ArrayDiscounts.xcplaygroundpage/Contents.swift
+++ DSA-LeetCode.playground/Pages/ArrayDiscounts.xcplaygroundpage/Contents.swift
@@ -26,3 +26,24 @@ func finalPrices(_ prices: [Int]) -> [Int] {
     result.append(prices[prices.count - 1])
     return result
 }
+
+func finalPricesStack(_ prices: [Int]) -> [Int] {
+    var result = prices
+    var stack = [Int]()
+    
+    for index in stride(from: prices.count - 1, through: 0, by: -1) {
+        let currentPrice = prices[index]
+        
+        while let last = stack.last, last > currentPrice {
+            _ = stack.removeLast()
+        }
+        
+        let discount = ((stack.last ?? Int.max) <= currentPrice) ? (stack.last ?? Int.max) : 0
+        
+        result[index] -= discount
+        
+        stack.append(currentPrice)
+    }
+    
+    return result
+}
