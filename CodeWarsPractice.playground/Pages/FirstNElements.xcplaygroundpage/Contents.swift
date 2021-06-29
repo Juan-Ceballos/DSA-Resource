ac68186dd1ba1d14bf904f1ed9a20eb73636a215
Juan C <juanceballos@pursuit.org>
2021-06-29 12:02:41 -0400

    up to n elements return arr linear

---
+++ CodeWarsPractice.playground/Pages/FirstNElements.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// constant time
+func take(_ arr: [Int], _ n: Int) -> [Int] {
+  return Array(arr.prefix(n))
+}
+
+// my solution O(n)
+func take(_ arr: [Int], _ n: Int) -> [Int] {
+  
+  guard !arr.isEmpty else {
+    return []
+  }
+  
+  if n >= arr.count {
+    return arr
+  }
+  
+  var result = [Int]()
+  
+  
+  for num in 0..<n {
+    result.append(arr[num])
+  }
+  
+  return result
+  
+}
+
