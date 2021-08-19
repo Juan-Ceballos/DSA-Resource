bf4360a7038d7c48e96991999b0cdfea4c090c0d
Juan C <juanceballos@pursuit.org>
2021-08-19 13:11:35 -0400

    codewars, sorted nth linked list

---
+++ CodeWarsPractice.playground/Pages/BubbleSort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func bubbleSort(arr: [Int]) -> [Int] {
+    var sortedArr = arr
+    
+    for i in 0..<arr.count {
+        
+        for j in i + 1..<arr.count - 1 {
+            
+        }
+    }
+    
+    return sortedArr
+}
