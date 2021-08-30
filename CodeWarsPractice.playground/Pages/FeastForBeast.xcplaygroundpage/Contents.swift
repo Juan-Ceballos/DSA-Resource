d90676e0f5ff151e72a174adf1abbc29ff1fdbc6
Juan C <juanceballos@pursuit.org>
2021-08-30 19:29:03 -0400

    codewars, feastbeast, index string

---
+++ CodeWarsPractice.playground/Pages/FeastForBeast.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+function feast(beast, dish) {
+
+  let beastArr = Array.from(beast)
+  let dishArr = Array.from(dish)
+  if (beastArr[0] === dishArr[0] && beastArr[beastArr.length - 1] === dishArr[dishArr.length - 1]) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(feast("great blue heron", "garlic naan"))
+
+// could index string in js
+*/
