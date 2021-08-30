634d161dc8c28bce2af1e42a4577d5d423607e47
Juan C <juanceballos@pursuit.org>
2021-08-30 17:15:56 -0400

    codewars, well of good ideas, concat matrix, iterate, conditinals

---
+++ CodeWarsPractice.playground/Pages/WellOfIdeas.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//: [Previous](@previous)
+
+import Foundation
+/*
+function well(x){
+  let tempArr = [].concat.apply([], x)
+  let goodCount = 0
+  for (let i = 0; i < tempArr.length; i++) {
+    let review = tempArr[i]
+    if (typeof review == "string") {
+      if (review.toLowerCase() === "good") {
+      goodCount += 1
+      }
+    }
+  }
+  
+  if (goodCount > 2) {
+    return "I smell a series!"
+  } else if (goodCount > 0) {
+    return "Publish!"
+  } else {
+    return "Fail!"
+  }
+  
+}
+*/
