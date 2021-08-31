fb94f6af3b88be91edd7493cd795e55c65910e72
Juan C <juanceballos@pursuit.org>
2021-08-30 23:02:27 -0400

    codewars, find smallest int in array, array interation

---
+++ CodeWarsPractice.playground/Pages/FindSmallestInt.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+function findSmallestInt(args) {
+    let firstIndexForNumberInArray = 0
+    let smallest = args[firstIndexForNumberInArray]
+    for (let i = 1; i < args.length; i++) {
+      let currentNumberInArray = args[i]
+      if (smallest > currentNumberInArray) {
+        smallest = currentNumberInArray
+      }
+    }
+    return smallest
+  }
+
+  console.log(findSmallestInt([78,56,232,12,8]))
+*/
