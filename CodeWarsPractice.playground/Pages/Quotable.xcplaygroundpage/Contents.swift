db024694b6984e678145a80320352ea9fa4e562a
Juan C <juanceballos@pursuit.org>
2021-08-30 20:46:32 -0400

    codewars, quotable, string escaping adding

---
+++ CodeWarsPractice.playground/Pages/Quotable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+/*
+function quotable(name, quote){
+  return name + ' said: ' + "\"" + quote + "\""
+}
+
+quotable('Grae', 'Practice makes perfect')
+*/
