4f20bd511ed1f1c87b18f0ac84d7557e1ba8cad5
Juan C <juanceballos@pursuit.org>
2021-09-02 02:18:28 -0400

    find magic index

---
+++ CodeWarsPractice.playground/Pages/MagicFindArr.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ function findMagic(arr){
+   let magic = -1
+   for (let i = 0; i < arr.length; i++) {
+     if (arr[i] == i) {
+       magic = i
+     }
+   }
+   
+   return magic
+ }
+
+ console.log(findMagic([-3,1,-4,5]))
+*/
