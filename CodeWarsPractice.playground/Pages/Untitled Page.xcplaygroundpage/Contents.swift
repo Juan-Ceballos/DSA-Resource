8eb0343609bbdfb1295a49c53974d6ca1fd155e6
Juan C <juanceballos@pursuit.org>
2021-09-02 02:37:19 -0400

    codewars, pos neg in arr

---
+++ CodeWarsPractice.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ function countPositivesSumNegatives(input) {
+   if (input === null || input === []) {
+     return []
+   }
+   
+   let pos = 0
+   let neg = 0
+   
+   for (let i = 0; i < input.length; i++) {
+     if (input[i] > 0) {
+       pos += 1
+     } else if (input[i] < 0) {
+       neg += input[i]
+     }
+   }
+   
+   if (neg === 0 && pos === 0 && input.length >= 0) {
+     console.log("hey")
+     return []
+   }
+   
+   return [pos, neg]
+   
+ }
+
+ console.log(countPositivesSumNegatives([1, 2, -3]))
+ */
