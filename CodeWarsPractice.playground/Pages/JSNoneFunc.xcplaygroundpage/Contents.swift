ea09e41a4e8d6a23d9d2e89e6ef70d54ed90dc11
Juan C <juanceballos@pursuit.org>
2021-09-10 13:46:49 -0400

    js none func

---
+++ CodeWarsPractice.playground/Pages/JSNoneFunc.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+function none(arr, fun){
+  for (let i = 0; i < arr.length; i++) {
+    if (fun(arr[i]) == true) {
+      return false
+    }
+  }
+  return true
+}
+*/
