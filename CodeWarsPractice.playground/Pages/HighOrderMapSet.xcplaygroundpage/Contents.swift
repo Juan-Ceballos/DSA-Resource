6749f7bc64dc5e29d637502363effc278000e055
Juan C <juanceballos@pursuit.org>
2021-09-10 14:33:57 -0400

    js, codewars, high order functions, sets maps

---
+++ CodeWarsPractice.playground/Pages/HighOrderMapSet.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ function isSameLanguage(list) {
+   let languageArr = list.map(student => student.language)
+   let languageSet = new Set(languageArr)
+   return languageSet.size == 1
+ }
+ */
