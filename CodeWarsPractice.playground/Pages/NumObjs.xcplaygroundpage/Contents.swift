77a1bfb59fb27e10b2ba3f63e66c91e54faabb8f
Juan C <juanceballos@pursuit.org>
2021-09-09 21:20:00 -0400

    codewars, num objs

---
+++ CodeWarsPractice.playground/Pages/NumObjs.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+/*
+function numObj(s){
+  let resultsArr = []
+  for (let i = 0; i < s.length; i++) {
+    let num = s[i].toString()
+    var resObj = {
+      [num]: String.fromCharCode(num)
+    };
+    resultsArr.push(resObj)
+  }
+  
+  return resultsArr
+}
+*/
