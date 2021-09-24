d538992c82ece5f0d9cc127bc33a8563cfb0a8ef
Juan C <juanceballos@pursuit.org>
2021-09-23 22:42:14 -0400

    js, codewars, sum all arrays, reduce, filter, typeof, recur

---
+++ CodeWarsPractice.playground/Pages/AddAllArrs.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//function arraySum(arr) {
+//  let temp = []
+//  for (let i = 0; i < arr.length; i++) {
+//    if (typeof arr[i] === 'number') {
+//      temp.push(arr[i])
+//    } else {
+//      if (typeof arr[i] === 'string' || typeof arr[i] === 'function') {
+//       temp.push(0)
+//      }
+//      else {
+//        temp = arr[i].reduce((a, b) => a.concat(b), temp)
+//      }
+//    }
+//  }
+//  temp = temp.filter((a) => typeof a === 'number')
+//  temp = temp.reduce((a, b) => a + b)
+//  return temp
+//
+//}
