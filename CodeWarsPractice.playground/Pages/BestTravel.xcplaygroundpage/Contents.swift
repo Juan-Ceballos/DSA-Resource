5a216dcd9a34803683d39bc1663234b172ed2f50
Juan C <juanceballos@pursuit.org>
2021-12-07 16:33:33 -0500

    best travel codewars problem done after reviewing

--- CodeWarsPractice.playground/Pages/BestTravel.xcplaygroundpage/Contents.swift
+++ CodeWarsPractice.playground/Pages/BestTravel.xcplaygroundpage/Contents.swift
@@ -43,6 +43,7 @@ func combinations(arr: [Int]) -> [[Int]] {
     let rest = Array(arr[1...])
     
     // recursive call, the not take route generated
+    // or generate all the not take options with rest
     let combsWithoutFirst = combinations(arr: rest)
     // the take route
     var combsWithFirst = [[Int]]()
@@ -58,3 +59,71 @@ func combinations(arr: [Int]) -> [[Int]] {
 }
 
 print(combinations(arr: [1, 2, 3]))
+
+/*
+ Notes when first doing problem
+      t = maxDistance >= 0
+      ls = list of distances ls.count >= 1
+      k = number of times in list willing to visit >= 1
+  
+      main condition:
+      has to drive to t towns
+      secondary:
+      as close to distance t
+      
+      note - try not to modify ls
+  
+      edges
+      max distance = 0
+      min l in ls > t
+      
+      store currK
+      sort and store ls
+      store currTotal
+      iterate through sorted ls while not done with arr or currK < k
+        - if currTotal - value < 0 continue
+        - else subtract value from currTotal
+        - add 1 to currK
+  
+      Check max by ks
+      sort store ls
+      iterate and grab next k - 1
+      add values
+      update max if <= t
+      if max still == 0 return -1 else return max
+    */
+
+// problem done after reviewing
+func chooseBestSum2(_ t: Int, _ k: Int, _ ls: [Int]) -> Int {
+  var max = -1
+  let allCombinations = combinations2(ls)
+  
+  for comb in allCombinations {
+    if comb.count == k {
+      let totalDistance = comb.reduce(0, +)
+      if totalDistance > max && totalDistance <= t {
+        max = totalDistance
+      }
+    }
+  }
+  
+  return max
+  
+}
+
+func combinations2(_ arr: [Int]) -> [[Int]] {
+  if arr.isEmpty {return [[]]}
+  
+  let first = arr[0]
+  let rest = Array(arr[1...])
+  
+  let combsWithoutFirst = combinations(rest)
+  var combsWithFirst = [[Int]]()
+  
+  for comb in combsWithoutFirst {
+    let combWithFirst = comb + [first]
+    combsWithFirst.append(combWithFirst)
+  }
+  
+  return combsWithoutFirst + combsWithFirst
+}
