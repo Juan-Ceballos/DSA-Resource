2f8f120ce7fb641ec9a0c16d6bc3764057b9bf78
Juan C <juanceballos@pursuit.org>
2021-12-16 14:22:11 -0500

    dynamic program practice, memoization fib

--- DynamicProg.playground/Pages/FibMemo.xcplaygroundpage/Contents.swift
+++ DynamicProg.playground/Pages/FibMemo.xcplaygroundpage/Contents.swift
@@ -12,3 +12,15 @@ func fib(n: Int) -> Int {
 }
 
 print(fib(n: 4))
+
+func fib2(_ n: Int, _ memo: inout [Int: Int]) -> Int {
+  if let fibVal = memo[n] {
+    return fibVal
+  }
+  if n == 0 {return 0}
+  else if n <= 2 {return 1}
+  memo[n] = fib2(n - 1, &memo) + fib2(n - 2, &memo)
+  return memo[n]!
+}
+var temp = [Int:Int]()
+print(fib2(4, &temp))
