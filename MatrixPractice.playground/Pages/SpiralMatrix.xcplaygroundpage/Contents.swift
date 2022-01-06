ebc377068b06a7df19d40bc3bbaa824131f8a81a
Juan C <juanceballos@pursuit.org>
2022-01-06 03:14:17 -0500

    spiral matrix

--- MatrixPractice.playground/Pages/SpiralMatrix.xcplaygroundpage/Contents.swift
+++ MatrixPractice.playground/Pages/SpiralMatrix.xcplaygroundpage/Contents.swift
@@ -95,4 +95,75 @@ func spiralMatrix(matrix: [[Int]]) -> [Int] {
     return result
 }
 
-print(spiralMatrix(matrix: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+//print(spiralMatrix(matrix: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+func spiralMatrix2(matrix: [[Int]]) -> [Int] {
+    var result = [Int]()
+    var firstRow = 0
+    var firstCol = 0
+    var lastRow = matrix.count - 1
+    var lastCol = matrix[0].count - 1
+    
+    var dir = "R"
+    var seen = Array(repeating: Array(repeating: false, count: 3), count: 3)
+    
+    while(firstRow <= lastRow && firstCol <= lastCol) {
+        if dir == "R" {
+            for i in (firstCol...lastCol) {
+                if seen[firstRow][i] == false {
+                    result.append(matrix[firstRow][i])
+                    seen[firstRow][i] = true
+                }
+            }
+            firstRow += 1
+            dir = "D"
+            print("r")
+        }
+        
+        else if dir == "D" {
+            for i in (firstRow...lastRow) {
+                if seen[i][lastCol] == false {
+                    result.append(matrix[i][lastCol])
+                    seen[i][lastCol] = true
+                }
+            }
+            lastCol -= 1
+            dir = "L"
+            print("d")
+
+        }
+        
+        else if dir == "L" {
+            for i in (firstCol...lastCol).reversed() {
+                if seen[lastRow][i] == false {
+                    result.append(matrix[lastRow][i])
+                    seen[lastRow][i] = true
+                }
+            }
+            lastRow -= 1
+            dir = "U"
+            print("l")
+
+        }
+        
+        else if dir == "U" {
+            for i in (firstRow...lastRow).reversed() {
+                if seen[i][firstCol] == false {
+                    result.append(matrix[i][firstCol])
+                    seen[i][firstCol] = true
+                }
+            }
+            firstCol += 1
+            dir = "R"
+            print("u")
+
+        }
+        
+    }
+    
+    return result
+}
+
+print(spiralMatrix2(matrix: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+
---
+++ MatrixPractice.playground/Pages/SpiralMatrixII.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func SpiralMatrixII(matrix: [[Int]]) -> [Int] {
+    let res = [Int]()
+    return res
+}
