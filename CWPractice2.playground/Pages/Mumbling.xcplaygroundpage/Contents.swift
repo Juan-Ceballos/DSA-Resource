981b1ed7f98ce349f277b298da73ae0e9f685117
Juan C <juanceballos@pursuit.org>
2022-01-27 13:16:34 -0500

    cw, accum, str manip, accumulating string pattern, joined, droplast

---
+++ CWPractice2.playground/Pages/Mumbling.xcplaygroundpage/Contents.swift
@@ -0,0 +1,88 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func accum(_ s: String) -> String {
+  var res = ""
+  for (index, char) in s.enumerated() {
+    let seqRes = repeatStr(String(char).lowercased(), index)
+    if index < s.count - 1 {
+        res += "\(String(char).uppercased())\(seqRes)-"
+    } else {
+        res += "\(String(char).uppercased())\(seqRes)"
+    }
+  }
+  
+  print(res)
+  return res
+}
+
+func repeatStr(_ s: String, _ n: Int) -> String {
+  var res = ""
+  for num in 0..<n {
+    res += s
+  }
+  return res
+}
+
+//------------------------
+
+func accum2(_ str: String) -> String {
+    return str.enumerated().map {
+        repeatElement(String($1), count: $0 + 1)
+            .joined()
+            .capitalized
+    }
+        .joined(separator: "-")
+}
+
+func accum3(_ str: String) -> String {
+    return str.enumerated().map {
+        String(repeating: $1, count: $0 + 1).capitalized
+    }.joined(separator: "-")
+}
+
+func accum4(_ s: String) -> String {
+    var result = ""
+    var count = 1
+    
+    for character in s {
+        result += "\(character)".uppercased()
+        for _ in 1..<count {
+            result += "\(character)".lowercased()
+        }
+        count += 1
+        result += s.count == count - 1  ? "" : "-"
+    }
+    
+    return result
+}
+
+func accum5(_ s: String) -> String {
+    var result = [String]()
+    
+    for (i, value) in s.lowercased().enumerated() {
+        var string = Array(repeating: value, count: i+1).map { String($0) }.joined().capitalized
+        result.append(string)
+    }
+    
+    return result.joined(separator: "-")
+}
+
+func accum6(_ s: String) -> String {
+    return String(s.enumerated().map { String(repeating: $1, count: $0 + 1).capitalized + "-" }.reduce("", +).dropLast())
+}
+
+func accum7(_ s: String) -> String {
+    var phrase = ""
+    var count = 1
+    for character in s {
+        let add = String(repeating: character, count: count).capitalized
+        phrase.append(add)
+        if count < s.count {
+          phrase.append("-")
+        }
+        count += 1
+    }
+    return phrase
+}
